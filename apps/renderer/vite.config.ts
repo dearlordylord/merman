cd5a9b0463907abaa1215a7832c3c5af66a229eb
Igor Loskutov <igor.loskutoff@gmail.com>
2025-01-22 11:55:52 -0500

    vercel build

--- apps/renderer/vite.config.ts
+++ apps/renderer/vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { nxCopyAssetsPlugin } from '@nx/vite/plugins/nx-copy-assets.plugin';
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
   root: __dirname,
@@ -14,7 +15,7 @@ export default defineConfig({
     port: 4300,
     host: 'localhost',
   },
-  plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md'])],
+  plugins: [nxViteTsPaths(), nxCopyAssetsPlugin(['*.md']), vercel()],
   // Uncomment this if you are using workers.
   // worker: {
   //  plugins: [ nxViteTsPaths() ],
