71bf1ce916e72d7b70cc0032b8f71201418325b0
Igor Loskutov <igor.loskutoff@gmail.com>
2025-01-22 12:26:04 -0500

    encoder url in renderer messages

---
+++ apps/renderer/.vercel/output/static/assets/stateDiagram-v2-4JROLMXI-BA0-esXB.js
@@ -0,0 +1,1 @@
+import{s as t,a as e,c as r,b as s}from"./chunk-7U56Z5CX-CkfU3F7l.js";import{_ as i}from"./index-0gsVZ72Z.js";import"./chunk-5HRBRIJM-SITuH1YV.js";var _={parser:t,db:e,renderer:r,styles:s,init:i(a=>{a.state||(a.state={}),a.state.arrowMarkerAbsolute=a.arrowMarkerAbsolute,e.clear()},"init")};export{_ as diagram};
